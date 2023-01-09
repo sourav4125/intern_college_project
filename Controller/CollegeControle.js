58b1df1b707454da92d7b1faeb665ce2f56c69b0
aditya0129 <adityakumar00129@gmail.com>
2023-01-09 15:09:46 +0530

    get

--- Controller/CollegeControle.js
+++ Controller/CollegeControle.js
@@ -2,14 +2,19 @@ const CollegeModel=require("../models/collegemodel")
 
 
 const createcollege=async function(req,res){
-    let data=req.body
+    try{ 
+        let data=req.body
     const {name,fullname,logolink}=data
     if(!data){
         res.status(400).send({msg:"all fiels are mandatory"})
     }
     let savedata=await CollegeModel.create(data)
-    
+
     res.status(201).send({status:true,data:savedata})
+}catch(error){
+    res.status(500).send({error:error.message})
+
+}
 }
 
 module.exports.createcollege=createcollege
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const interController = require("../controller/interncontroller");
+const collegecontrollr=require("../Controller/CollegeControle")
 
 router.post("/functionup/interns", interController.new_intern);
+
+router.post("/functionup/colleges",collegecontrollr.createcollege)
+
+router.get("/functionup/collegeDetails")
 
 module.exports = router;
