11baec84036de7d2bdb4bd91146d186452df6cfa
aditya0129 <adityakumar00129@gmail.com>
2023-01-09 12:37:14 +0530

    interModel

---
+++ models/InternModel.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose")
+const ObjectId=mongoose.Schema.Types.ObjectId
+
+
+
+const InternModel=new mongoose.Schema(
+    {
+         name: {
+            type:String,
+            required:true
+        },
+          email: {
+            required:true,
+            unique:true
+             },
+           mobile: {
+            type:Number,
+           required:true, 
+           unique:true
+        }, 
+           collegeId: {
+            type:ObjectId,
+            ref:"CollegeData"
+
+           } ,
+            isDeleted: {
+                type:boolean, 
+                default: false
+            }
+
+        },{timestamps:true})
+
+        module.exports=mongoose.model("InternData",InternModel)
