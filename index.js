f48552fd793f5245c18799a3cc283111f5ff8e19
Soumyaranjanp68 <soumyaranjanp68@gmail.com>
2023-01-09 12:38:58 +0530

    index

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("HELLO ALL")
+const express = require ("express");
+const app = express();
+
+app.use(express.json());
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose.connect("",{useNewUrlParser: true})
+.then(()=> console.log("MongoDB is connected")) 
+.catch(err => console.log(err));
+
+const route = require('./routes/route');
+app.use('/',route);
+
+app.listen(process.env.PORT||3000, function(){
+    console.log("app is running on" + (process.env.PORT || 3000))
+});
